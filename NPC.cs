4c178f36f086a7b8c88b51e838c35ab2b121c6b5
BlackPhoenix21 <blackphoenix21.dark@gmail.ocm>
2025-01-15 19:10:33 +0100

    Fixed Scripts

--- NPC.cs
+++ NPC.cs
@@ -5,14 +5,12 @@ using TMPro;
 
 public class NPC : MonoBehaviour
 {
-    public string newText = "Hello World!";
     public GameObject text;
-
-
-    public TMP_Text NPC_text;
-
-    float timer = 0;
-    private object displayText;
+    bool npc = false;
+    public TMP_Text NPC_TextOut;
+    public static float timer = 0;
+    public static float timerlenght = 0;
+    public static string NPC_TextIn;
 
     // Start is called before the first frame update
     void Start()
@@ -23,33 +21,60 @@ public class NPC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if (timer > displayText.Length - 1)
-        {
-            text.GetComponent<TMP_Text>().text = displayText.Substring(0, (int)timer);
-
-        }
-        else
+        if (npc)
+            timer -= Time.deltaTime;
+        if (timer < 0)
         {
             text.SetActive(false);
-            //NPC_text.text = "haha du loser";
+            npc = false;
+            timerlenght = 0;
+        }
+    }
 
+    IEnumerator Typing()
+    {
+        int i = -1;
+        float zeit = 0.1f;
+        while (!NPC_TextOut.text.ToString().Equals(NPC_TextIn))
+        {
+            string typ = NPC_TextIn.Substring(i + 1);
+            string typadd = typ.Substring(0, 1);
+            NPC_TextOut.text += typadd;
+            yield return new WaitForSeconds(zeit);
+            i++;
         }
 
-
-
-
     }
+
+    public int CoinSearch()
+    {
+        GameObject player = GameObject.FindWithTag("Player");
+        int coin = 0;
+        coin = player.GetComponent<Purse>().coins;
+        return coin;
+    }
+
+    public static void Textzeit()
+    {
+        for (int i = 0; i < NPC_TextIn.Lenght; i++)
+        {
+            timerlenght += 0.2f;
+        }
+    }
+
     void OnMouseOver()
     {
         if (Input.GetMouseButtonUp(0))
         {
+            int coin = CoinSearch();
+            NPC_TextOut.text = "Du hast " + coin + " Münzen!";
+            NPC_TextIn = NPC_TextOut.text.ToString();
+            NPC_TextOut.text = "";
+            Textzeit();
+            timer = timerlenght;
             text.SetActive(true);
-            text.GetComponent<TMP_Text>().text = "";
-            displayText = "";
+            npc = true;
+            StartCoroutine(Typing());
         }
-
-
     }
 }
--- Purse.cs
+++ Purse.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Purse : MonoBehaviour
 {
-    public= int coins = 0;
+    public int coins = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,17 +14,15 @@ public class Purse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-
         if (other.gameObject.tag == "Coin")
         {
             Destroy(other.gameObject);
             coins++;
         }
-
     }
 }
